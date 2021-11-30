bd380a452aecf3db68883b0d9846e33dfa0db912
adrielrobles <adropere@espol.edu.ec>
2021-11-30 01:53:51 -0500

    Buscador de categorias de bateria

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,3 +1,116 @@
+function cambiohtml(producto) {
+    let html =
+        `<div class="col-md-4 col-sm-6 bateria">
+    <div class="baterie-item wow fadeIn animated" data-wow-duration="0.75s" style="visibility: visible;-webkit-animation-duration: 0.75s; -moz-animation-duration: 0.75s; animation-duration: 0.75s;">
+        <div class="thumb-content">
+            <div class="thumb-inner">
+                <img src="${producto.src}" alt="">
+            </div>
+            <div class="baterie-banner">
+                <a>${producto.tipo}</a>
+            </div>
+        </div>
+        <div class="down-content">
+            <h4>${producto.nombre}</h4>
+            <span>${producto.precio}</span>
+            <div class="descripcion row">
+                <p class="col-12">Caracteristicas:</p>
+                <p class="col-12">${producto.caracteristicas[0]}</p>
+                <p class="col-12">${producto.caracteristicas[1]}</p>
+                <p class="col-12">${producto.caracteristicas[2]}</p>
+            </div>
+
+            <ul class="baterie-info">
+                <li>
+                    <div class="button item"><i class="bi bi-cart3"></i>
+                        <p>Añadir al carrito</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>`
+    return html;
+}
+let peticion = async() => {
+    let htmlC = ``;
+    try {
+        let response = await fetch('bateria.json');
+        const data = await response.json();
+        data.Productos.forEach(producto => {
+            let html = cambiohtml(producto)
+            htmlC += html;
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+    document.getElementsByClassName("catalogo_bateria")[0].innerHTML = htmlC
+
+}
+document.addEventListener("DOMContentLoaded", function(event) {
+    peticion();
+})
+
+function filtrado() {
+    const boton = document.getElementsByClassName("form-control rounded");
+    let texto_boton = boton[0].value;
+    let tipo = document.getElementById("navproductos").getElementsByClassName("tm-paging-link active")[0].innerHTML
+    fetch("bateria.json")
+        .then(response => response.json())
+        .then(data => {
+            let htmlC = ``;
+            data.Productos.forEach(producto => {
+                let descripcion = producto.caracteristicas[0] + producto.caracteristicas[1] + producto.caracteristicas[2] + " " + producto.precio + " " + producto.nombre
+                if (tipo == producto.tipo) {
+                    if (descripcion.toLowerCase().includes(texto_boton.toLowerCase())) {
+                        let html = cambiohtml(producto)
+                        htmlC += html;
+                    }
+                } else {
+                    if (descripcion.toLowerCase().includes(texto_boton.toLowerCase()) && tipo == "Todos los Productos") {
+                        let html =
+                            cambiohtml(producto)
+                        htmlC += html;
+                    }
+                }
+            });
+            document.getElementsByClassName("catalogo_bateria")[0].innerHTML = htmlC
+        })
+        .catch(console.error);
+}
+
+const buscar = () => {
+    const boton = document.getElementsByClassName("form-control rounded");
+    boton[0].addEventListener("keyup", filtrado, false);
+
+}
+buscar()
+
+let cambioproductos = async(a) => {
+    document.getElementsByClassName("catalogo_bateria")[0].innerHTML = ``
+    let htmlC = ``;
+    try {
+        let response = await fetch('bateria.json');
+        const data = await response.json();
+        data.Productos.forEach(producto => {
+            if (a.innerHTML == producto.tipo) {
+                let html =
+                    cambiohtml(producto)
+                htmlC += html;
+            }
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+    if (a.innerHTML != "Todos los Productos") {
+        document.getElementsByClassName("catalogo_bateria")[0].innerHTML = htmlC
+    } else {
+        peticion()
+    }
+
+}
 let cambiocatalogo = () => {
     let butonesa = document.getElementById("navproductos").getElementsByTagName("a")
     for (let a of butonesa) {
@@ -9,18 +122,7 @@ let cambiocatalogo = () => {
                     marca.classList.remove("active")
                 }
             }
-            let cat_marcas = document.getElementsByClassName("tm-gallery-page")
-            for (let marca of cat_marcas) {
-                if (a.innerHTML == "Todos los Productos") {
-                    marca.classList.remove("hidden")
-                } else {
-                    if (marca.getAttribute("id").includes(a.innerHTML)) {
-                        marca.classList.remove("hidden")
-                    } else {
-                        marca.classList.add("hidden")
-                    }
-                }
-            }
+            cambioproductos(a);
         })
     }
 
