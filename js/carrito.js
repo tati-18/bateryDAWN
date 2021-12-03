bff06320511ca85e0486fcef4dbffad25ddeeb50
tati-18 <tatyvera@espol.edu.ec>
2021-12-02 22:48:34 -0500

    agg Jqueris

--- js/carrito.js
+++ js/carrito.js
@@ -3,9 +3,10 @@ function recuperarValue(venta, productos) {
     let html =``
     for(let p of productos){
         if(p.id==venta.id){
+            html+=`<option value="${0}" selected>${0}</option>`
             for(i=1; i<=p.cantidad; i++){
                 if(venta.cantidad==i){
-                 html += `<option value="${i}" selected>${i}</option>`
+                 html += `<option value="${i}">${i}</option>`
                 }
                 else{
                     html += `<option value="${i}">${i}</option>`
@@ -33,15 +34,37 @@ function cambiohtml(ventas,producto) {
     <td><h3>${product.nombre}</h3></td>
     <td class="text-center mob-hide "><a href="" class="trsn" title=""><img src="${product.src}" alt="" title=""></a></td>
     <td class="mob-hide"> <span class="order-product-price">${product.precio}</span></td>
-    <td><select class="select select-qty form-control" name="89459851" title="Qty">
+    <td><select class="select select-qty form-control"  id="${product.id}" title="Qty">
     ${recuperarValue(ventas,producto)}
     </td>
-    <td><span class="order-product-subtotal">$${(ventas.cantidad)*(parseInt(product.precio.replace("$"," ")))}</span></td>
+    <td><span id="S${product.id}"class="order-product-subtotal">$0</span></td>
     <td clas="text-right"> <a href="" class="cart-product-remove" title="Remove Product"><i class="fas fa-times-circle"></i></a></td>
     </tr>`
     return html;
 }
 
+function ObtenerSubtotal(precio, id){
+    let selValue = document.querySelector(`#${id}`).value;
+    let product= recuperarValueO(ventas,producto)
+    let sub= (selValue)*(parseInt(precio.replace("$"," ")))
+    return sub;
+}
+
+ function click (data, producto){
+    for(let p of data){
+        let pro = recuperarValueO(p,producto)
+        let selValue = document.getElementById(`${p.id}`);
+        console.log(selValue)
+        selValue.addEventListener("change",()=>{
+            let cantidad = parseInt(selValue.value)
+            let precio = parseInt((pro.precio.replace("$",""))) 
+            let total = cantidad * precio
+            let span = document.getElementById(`S${p.id}`)
+            span.innerHTML="$"+total
+        })
+
+    }
+ }
 
 
 let peticion = async() => {
@@ -52,16 +75,16 @@ let peticion = async() => {
         data.Ventas.forEach(ventas => {
             let html = cambiohtml(ventas,data.Productos)
             htmlC += html;
-
+            
         })
         ;
+    document.getElementsByTagName("tbody")[0].innerHTML = htmlC
+    click(data.Ventas, data.Productos)
 
     } catch (error) {
         console.log(error);
-    }
-    document.getElementsByTagName("tbody")[0].innerHTML = htmlC
+    } 
     
-
 }
 document.addEventListener("DOMContentLoaded", function(event) {
     peticion();
@@ -70,7 +93,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
 
-  
+function activar(){
+
+
+
+}
+    
+
+
+    
+    
+
+
 
 
 
